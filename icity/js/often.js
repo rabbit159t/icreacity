71377bc14cc86b4f4ba49c05040b138754a1e589
Eddie <encorew56527@gmail.com>
2017-03-20 16:52:51 +0800

    no message

---
+++ icity/js/often.js
@@ -0,0 +1,9 @@
+var s = skrollr.init();
+
+$(window).scroll(function(evt){
+  if ($(window).scrollTop()>0)
+    $(".navbar").removeClass("navbar-top")
+  else
+ $(".navbar").addClass("navbar-top")
+
+});
